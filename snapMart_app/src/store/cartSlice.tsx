4a8e83e7492183fad899bc212e0ca75551758518
Jette kerz E. Castillo <jette.castillo@stationfive.com>
2023-06-17 22:05:46 +0800

    Add items to localstorage to persist the data

--- snapMart_app/src/store/cartSlice.tsx
+++ snapMart_app/src/store/cartSlice.tsx
@@ -1,46 +1,59 @@
-import { createSlice,PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface CartItem {
-    id : string
-    productName : string
-    description : string
-    unitPrice : number
-    category : string
-    imageUrl : string
-    quantity : number
-  }
+  id: string;
+  productName: string;
+  description: string;
+  unitPrice: number;
+  category: string;
+  imageUrl: string;
+  quantity: number;
+}
 
-  const cartSlice = createSlice({
-    name: 'cart',
-    initialState: [] as CartItem[],
-    reducers: {
-      addToCart: (state, action: PayloadAction<CartItem>) => {
-        const item = action.payload;
-        const existingItem = state.find((cartItem) => cartItem.id === item.id);
-        if (existingItem) {
-          existingItem.quantity += item.quantity;
-        } else {
-          state.push(item);
-        }
-      },
-      updateQuantity: (state, action: PayloadAction<{ itemId: string; quantity: number }>) => {
-        const { itemId, quantity } = action.payload;
-        const item = state.find((cartItem) => cartItem.id === itemId);
-        if (item) {
-          item.quantity += quantity;
-          if (item.quantity <= 0) {
-            return state.filter((cartItem) => cartItem.id !== itemId);
-          }
-        }
-      },
-      removeFromCart: (state, action: PayloadAction<string>) => {
-        const itemId = action.payload;
-        return state.filter((cartItem) => cartItem.id !== itemId);
-      },
-      clearCart: () => [],
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: [] as CartItem[],
+  reducers: {
+    locaStorageCart: (state, action: PayloadAction<CartItem[]>) => {
+      const item = action.payload;
+     return item
+
     },
-  });
-  
-  export const { addToCart, updateQuantity, removeFromCart, clearCart } = cartSlice.actions;
-  
-  export default cartSlice.reducer;
+    addToCart: (state, action: PayloadAction<CartItem>) => {
+      const item = action.payload;
+      const existingItem = state.find((cartItem) => cartItem.id === item.id);
+      if (existingItem) {
+        existingItem.quantity += item.quantity;
+      } else {
+        state.push(item);
+      }
+      AsyncStorage.setItem('cartItems', JSON.stringify(state));
+    },
+    updateQuantity: (state, action: PayloadAction<{ itemId: string; quantity: number }>) => {
+      const { itemId, quantity } = action.payload;
+      const item = state.find((cartItem) => cartItem.id === itemId);
+      if (item) {
+        item.quantity += quantity;
+        if (item.quantity <= 0) {
+          return state.filter((cartItem) => cartItem.id !== itemId);
+        }
+      }
+      AsyncStorage.setItem('cartItems', JSON.stringify(state));
+    },
+    removeFromCart: (state, action: PayloadAction<string>) => {
+      const itemId = action.payload;
+      const updatedState = state.filter((cartItem) => cartItem.id !== itemId);
+      AsyncStorage.setItem('cartItems', JSON.stringify(updatedState));
+      return updatedState;
+    },
+    clearCart: () => {
+      AsyncStorage.removeItem('cartItems');
+      return [];
+    },
+  },
+});
+
+export const {locaStorageCart, addToCart, updateQuantity, removeFromCart, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
